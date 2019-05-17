15739b4e8d2a7a1f75a6d6950e442cc4f91592f3
Alexey <lexahnure@gmail.com>
2019-05-17 10:01:07 +0300

    working minimize

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
 import './App.scss';
 import MenuItem from './components/menu/index';
 
 const itemsList=[
   {
     icon: 'face',
-    text: 'Mune item'
+    text: 'Android',
+    address: 'face'
   },
   {
-    icon: 'face',
-    text: 'Mune item'
+    icon: 'android',
+    text: 'Android',
+    address: 'android'
   },
 ];
 
 function App() {
   return (
     <div className="App">
+      <Router>
         <MenuItem itemsList={itemsList}/>
+      </Router>
     </div>
   );
 }
--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,23 +1,40 @@
 import React from 'react';
-import logo from '../../images/logo.png'
+import { NavLink } from 'react-router-dom';
+import logo from '../../images/logo.png';
 import './menu.scss';
 
 class MenuItem extends React.Component {
+  state = {
+    minimized: false,
+  }
+
+  minimizeHandler = () => {
+    this.setState({ minimized: !this.state.minimized });
+  }
 
   render() {
     const { itemsList } = this.props;
+    const { minimized } = this.state;
+
     return (
-      <div className="menu">
+      <div className={ minimized ? 'menu minimized' : 'menu' }>
         <div className="logo">
-          <img src={logo} alt="logo" />
+          <span>
+            <img src={logo} alt="logo" />
+          </span>
         </div>
+        <button type="button" className="btn btn-ghost btn-icon btn-round minimize" onClick={this.minimizeHandler}>
+          <i class="material-icons">{ minimized ? 'chevron_right' : 'chevron_left' }</i>
+        </button>
         <ul>
           {
             itemsList && itemsList.map((item, index) => {
               return  (
                 <li className="menu-item" key={index}>
-                  <i className="material-icons">{ item.icon || 'android' }</i>
-                  <span>{ item.text || 'Some text here' }</span>
+                  <NavLink to={`/${item.address}`}>
+                    <i className="material-icons">{ item.icon || 'android' }</i>
+                    <span>item.text</span>
+                  </NavLink>
                 </li>
               );
             })
